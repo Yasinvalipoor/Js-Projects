7029a0d74b15ad22bf41866907b414d2a86a2ab0
Yasinvalipoor <yasinvalipoor14@gmail.com>
2024-09-20 18:40:25 +0330

    add TurnOn-Off-Bulb project

---
+++ JavaScript/TurnOn-Off-Bulb/source/js/script.js
@@ -0,0 +1,17 @@
+﻿const $ = document;
+const imgElement = $.getElementById("bulb");
+const btnElement = $.getElementById("btn");
+
+let bulbFlag = false;
+
+function turnOffOrOn() {
+    if (bulbFlag) {
+        imgElement.setAttribute("src", "pics/bulboff.gif");
+        btnElement.innerHTML = "Turn On";
+        bulbFlag = false;
+    } else {
+        imgElement.setAttribute("src", "pics/bulbon.gif");
+        btnElement.innerHTML = "Turn Off";
+        bulbFlag = true;
+    }
+}
