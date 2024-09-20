514e96ce4ad53ef98357abf09669af78ba4e04ae
Yasinvalipoor <yasinvalipoor14@gmail.com>
2024-09-20 11:47:04 +0330

    fix DraggableTodoList

--- JavaScript/DraggableTodoList  خفن/js/app.js
+++ JavaScript/DraggableTodoList  خفن/js/app.js
@@ -3,34 +3,54 @@ const modalElement = $.getElementById('todo_form');
 const closeBtnModalElement = $.querySelector('.close-modal');
 const addBtnTodoElement = $.getElementById('add_btn');
 const overlayElement = $.getElementById('overlay');
+const todoInputElement = $.getElementById('todo_input');
+const todoFormElement = $.getElementById('body-form');
+const noStatusBoxElement = $.getElementById('no_status');
 
-// console.log(addBtnElement);
-
-
+//Show Modal Method
 function showModalHandler() {
     overlayElement.style.display = 'block'
     modalElement.style.top = '15%';
-}
+};
 
 function closeBtnModalHandler(){
     modalElement.style.top = '-50%';
     overlayElement.style.display = 'none'
-}
+};
 
 function keyCloseModalHandler(event) {
-    if (event.key == "Escape") {
-        l.style.top = '-50%';
+    if (event.key === "Escape") {
+        modalElement.style.top = '-50%';
         overlayElement.style.display = 'none'
     }
-}
+};
 
-// function onScreenCloseModalHandler(){
-//     console.log('کلیک شد');
+//Add New Todo To No Status Box
+function addNewTodo(newTodoValue) {
+    let newTodoDiv = $.createElement("div");
+    newTodoDiv.className ="todo";
+    newTodoDiv.innerHTML = newTodoValue;
+
+    let newTodoSpan = $.createElement("span");
+    newTodoSpan.className ="close";
+    newTodoSpan.innerHTML = '&times;';
+    newTodoDiv.append(newTodoSpan);
+
+    noStatusBoxElement.append(newTodoDiv);
+};
+
+function addNewTodoHandler(event) {
+    event.preventDefault(); // Prevent form submission from reloading the page
+    let newTodoValue = todoInputElement.value.trim();
     
-//     // modalElement.style.top = '-50%';
-// }
+    if (newTodoValue) {
+        addNewTodo(newTodoValue);
+        todoInputElement.value = ''; // Clear the input field
+    }
+};
 
 addBtnTodoElement.addEventListener('click', showModalHandler)
 closeBtnModalElement.addEventListener('click', closeBtnModalHandler)
-document.body.addEventListener('keydown', keyCloseModalHandler)
-// document.body.addEventListener('click', onScreenCloseModalHandler)
+$.body.addEventListener('keydown', keyCloseModalHandler)
+todoFormElement.addEventListener('submit', addNewTodoHandler)
+
