dd0545b3595c7992f0df93085e3a6232ee7ebf17
Yasinvalipoor <yasinvalipoor14@gmail.com>
2024-09-21 13:24:17 +0330

    Ticket Reserve Project

---
+++ JavaScript/TicketReservation/js/script.js
@@ -0,0 +1,24 @@
+﻿const $ = document;
+const countrySelectElement = $.querySelector(".countrySelect");
+const citySelectElement = $.querySelector(".citySelect");
+
+const countryCities = {
+    Iran: ["Isfahan", "Tehran", "Shiraz", "Mashhad", "Yazd", "Kerman", "Qom", "Rasht", "Ahvaz", "Tabriz"],
+    Canada: ["Toronto", "Vancouver", "Montreal", "Calgary", "Ottawa", "Edmonton", "Quebec City", "Winnipeg", "Hamilton", "Victoria"],
+    US: ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose"]
+};
+
+countrySelectElement.addEventListener("change", function (event) {
+    if (countrySelectElement.value == 'Please Select') {
+        citySelectElement.innerHTML = '';
+        let optionElement = `<option value="Please Select Country">Please Select Country</option>`;
+        citySelectElement.innerHTML = optionElement;
+    } else {
+        let city = countryCities[countrySelectElement.value];
+        citySelectElement.innerHTML = '';
+        city.forEach(element => {
+            let optionElement = `<option class="option" value="${element}">${element}</option>`;
+            citySelectElement.innerHTML += optionElement;
+        });
+    }
+});
