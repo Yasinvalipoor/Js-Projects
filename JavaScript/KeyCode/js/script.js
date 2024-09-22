f9652171344953bbcb60fb2200a3fe58127b5f0d
Yasinvalipoor <yasinvalipoor14@gmail.com>
2024-09-22 13:27:45 +0330

    KeyCode Information

---
+++ JavaScript/KeyCode/js/script.js
@@ -0,0 +1,19 @@
+﻿const $ = document;
+const title = $.getElementById("keyCode");
+const locationKey = $.getElementById("location");
+const key = $.getElementById("key");
+const which = $.getElementById("which");
+const code = $.getElementById("code");
+
+document.body.addEventListener("keydown", function (event) {
+	starter.style.display = "none";
+	heading.style.display = "flex";
+	ascii.style.display = "flex";
+	infos.style.display = "flex";
+
+	title.innerHTML = event.code;
+	locationKey.innerHTML = event.location;
+	key.innerHTML = event.key;
+	which.innerHTML = event.which;
+	code.innerHTML = event.code;
+});
