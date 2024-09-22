00ac983e55a1255c689966eb636d0e5bf1ccad3e
Yasinvalipoor <yasinvalipoor14@gmail.com>
2024-09-22 16:37:57 +0330

    Show Password

--- JavaScript/ShowPassword/js/script.js
+++ JavaScript/ShowPassword/js/script.js
@@ -1,24 +1,24 @@
-﻿ // Functions for select elems with id & class
- var $ = document;
- function _id(id_name) {
-   return $.getElementById(id_name);
- }
+﻿// Functions for select elems with id & class
+var $ = document;
+function _id(id_name) {
+  return $.getElementById(id_name);
+}
 
- function _class(class_name) {
-   return $.getElementsByClassName(class_name);
- }
+function _class(class_name) {
+  return $.getElementsByClassName(class_name);
+}
 
- // Select Elems
- var togglePassword = _class("toggle-password");
- var passwordField = _id("password-field");
+// Select Elements
+var togglePassword = _class("toggle-password")[0];
+var passwordField = _id("password-field");
 
- // Fire click event on eye icon
- togglePassword.addEventListener("click", function () {
-   if (passwordField.type == "text") {
-     passwordField.type = "pasword";
-     togglePassword.classList.remove("active");
-   } else {
-     passwordField.type = "text";
-     togglePassword.classList.add("active");
-   }
- })
+// Fire click event on eye icon
+togglePassword.addEventListener("click", function () {  
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    togglePassword.classList.add("active");
+  } else {
+    passwordField.type = "password";
+    togglePassword.classList.remove("active");
+  }
+})
