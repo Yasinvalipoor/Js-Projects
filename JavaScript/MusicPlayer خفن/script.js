53a68bbd74349e6dca3bbaeeb7eaec14026f885b
Yasinvalipoor <yasinvalipoor14@gmail.com>
2024-09-23 16:51:46 +0330

    Music Player UI - Best Project

--- JavaScript/MusicPlayer خفن/script.js
+++ JavaScript/MusicPlayer خفن/script.js
@@ -9,6 +9,13 @@ const playerTrack = _id("player-track");
 // Times
 const currentTimeDisplay = _id("current-time");
 const totalDurationDisplay = _id("track-length");
+// Audio - Source 
+const audioElement = _id("audio-element");
+const trackNameElement = _id("track-name");
+const singerNameElement = _id("singer-name");
+// Cover
+const imgAlbumCoverElement = _id("album-cover");
+const imgBgArtworkElement = _id("bg-artwork");
 // SeekBar
 var seekBar = _id("seek-bar");
 var sArea = _id("s-area");
@@ -17,35 +24,44 @@ var sHover = _id("s-hover");
 // Icons
 const faPlay = $.querySelector(".fa-play");
 const faPause = $.querySelector(".fa-pause");
-const audioElement = $.querySelector("audio");
+const sourceElement = $.querySelector("source");
 // Sources array
 const musicSrc = [
-    "DMT-Ashkan-Kagan.mp3",
-    "Precious-Little-Hiatus.mp3",
-    "Secret-Whispers-Behdad-Bahrami.mp3",
+    "DMT-Ashkan Kagan.mp3",
+    "Precious Little-Hiatus.mp3",
+    "Secret Whispers-Behdad Bahrami.mp3",
 ];
 // Variable
 var musicIndex = 0;
 var duration = 0;
+var audioFile = sourceElement.src;
+var jsmediatags = window.jsmediatags;
+
+
 
 
 
 play_PauseBtn.addEventListener("click", function () {
     togglePlayPause();
+    updateCoverAndTags(sourceElement.src);
 });
 
 playPreviousBtn.addEventListener("click", function () {
     musicIndex--;
     if (musicIndex < 0) { musicIndex = 2; } // Condition
-    audioElement.setAttribute("src", `Audio/${musicSrc[musicIndex]}`);
+    sourceElement.setAttribute("src", `Audio/${musicSrc[musicIndex]}`);
+    audioElement.load();
     audioElement.play();
+    updateCoverAndTags(sourceElement.src);
     if (faPause.classList.contains("hidden-display")) { togglePlayPause(); } // Condition
 });
 
 playNextBtn.addEventListener("click", function () {
     musicIndex++;
     if (musicIndex > musicSrc.length - 1) { musicIndex = 0; } // Condition
-    audioElement.setAttribute("src", `Audio/${musicSrc[musicIndex]}`);
+    sourceElement.setAttribute("src", `Audio/${musicSrc[musicIndex]}`);
+    audioElement.load();
+    updateCoverAndTags(sourceElement.src); // به‌روزرسانی کاور آهنگ جدید
     audioElement.play();
     if (faPause.classList.contains("hidden-display")) { togglePlayPause(); } // Condition
 });
@@ -53,16 +69,34 @@ playNextBtn.addEventListener("click", function () {
 
 
 
-audioElement.addEventListener("loadedmetadata", function() {
+audioElement.addEventListener("loadedmetadata", function () {
     duration = audioElement.duration;
     totalDurationDisplay.textContent = formatTime(audioElement.duration);
+    // Solution 1
+    // var fileName = audioElement.querySelector("source").src.split("/").pop();
+    // fileName = decodeURIComponent(fileName.split(".")[0].split('-'));
+    // if (fileName.split(',').length === 2) {
+    //     var artistName = fileName.split(',')[0];
+    //     var songTitle = fileName.split(',')[1];
+    //     trackNameElement.textContent = songTitle;
+    //     singerNameElement.textContent = artistName;
+    // }
+    // Solution 2
+    // var fileName = audioElement.querySelector("source").src.split("/").pop();
+    // fileName = decodeURIComponent(fileName.split(".")[0]); // Decode the file name and remove the extension
+    // // Split the file name by the hyphen and trim whitespace
+    // var [songTitle, artistName] = fileName.split('-').map(part => part.trim());
+    // if (songTitle && artistName) {
+    //     trackNameElement.textContent = songTitle;
+    //     singerNameElement.textContent = artistName;
+    // }
 });
 
-audioElement.addEventListener("timeupdate", function() {
+audioElement.addEventListener("timeupdate", function () {
     var currentTime = audioElement.currentTime;
     currentTimeDisplay.textContent = formatTime(audioElement.currentTime);
     var progress = (currentTime / duration) * 100;
-    seekBar.style.width = progress + "%";    
+    seekBar.style.width = progress + "%";
     if (duration === currentTime) {
         currentTimeDisplay.textContent = '0:00';
         seekBar.style.width = 0 + "%";
@@ -72,7 +106,7 @@ audioElement.addEventListener("timeupdate", function() {
 
 
 // Mouse movement event on the search bar to display the corresponding time
-sArea.addEventListener("mousemove", function(event) {
+sArea.addEventListener("mousemove", function (event) {
     var sAreaWidth = sArea.offsetWidth;
     var offsetX = event.offsetX;
     var hoverTime = (offsetX / sAreaWidth) * duration;
@@ -88,7 +122,7 @@ sArea.addEventListener("mousemove", function(event) {
     sHover.style.display = "block";
 });
 
-sArea.addEventListener("mouseleave", function() {
+sArea.addEventListener("mouseleave", function () {
     sHover.style.display = "none";
     insTime.style.display = "none";
     insTime.style.left = "0px";
@@ -98,7 +132,7 @@ sArea.addEventListener("mouseleave", function() {
 
 
 // Click event to set playback time
-sArea.addEventListener("click", function(event) {
+sArea.addEventListener("click", function (event) {
     var sAreaWidth = sArea.offsetWidth;
     var offsetX = event.offsetX;
     var clickTime = (offsetX / sAreaWidth) * duration;
@@ -109,8 +143,8 @@ sArea.addEventListener("click", function(event) {
 function formatTime(seconds) {
     var minutes = Math.floor(seconds / 60);
     var seconds = Math.floor(seconds % 60);
-    if (seconds < 10) {seconds = "0" + seconds;}
-    if (minutes < 10) {minutes = "0" + minutes;}
+    if (seconds < 10) { seconds = "0" + seconds; }
+    if (minutes < 10) { minutes = "0" + minutes; }
     return minutes + ":" + seconds;
 }
 function togglePlayPause() {
@@ -120,5 +154,30 @@ function togglePlayPause() {
     albumArt.classList.toggle("active");
     faPlay.classList.contains("hidden-display") ? audioElement.play() : audioElement.pause(); // Condition
 }
-function _id(id_name) {return $.getElementById(id_name);}
-function _class(class_name) {return $.getElementsByClassName(class_name);}
+function _id(id_name) { return $.getElementById(id_name); }
+function _class(class_name) { return $.getElementsByClassName(class_name); }
+// A function to read the song information and display the cover
+function updateCoverAndTags(audioFile) {
+    jsmediatags.read(audioFile, {
+        onSuccess: function (tag) {
+            var tags = tag.tags;
+            if (tags.picture) {
+                var imageData = tags.picture.data;
+                var base64String = "";
+                for (var i = 0; i < imageData.length; i++) {
+                    base64String += String.fromCharCode(imageData[i]);
+                }
+                var base64Image = `data:${tags.picture.format};base64,${window.btoa(base64String)}`;
+                imgBgArtworkElement.style.backgroundImage = `url('${base64Image}')`;
+                imgAlbumCoverElement.src = base64Image;
+                singerNameElement.textContent = tag.tags.artist;
+                trackNameElement.textContent = tag.tags.title;
+            } else {
+                console.log("Information Tags Not Found");
+            }
+        },
+        onError: function (error) {
+            console.log('Error: ', error.type, error.info);
+        }
+    });
+}
